7fbad5f362509a4720bd2201fbb6547c3ac7b03d
Darker <wkc419@hotmail.com>
2020-04-10 19:54:15 +0100

    no message

---
+++ Assets/SplashScreen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    [SerializeField] float LevelLoadDelayTiome = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveToNextLevel();
+    }
+
+    private void MoveToNextLevel()
+    {
+        Invoke("LoadNextLevel", LevelLoadDelayTiome);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
