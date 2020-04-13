fdf845ebb8583158390057b950315513f3796645
Darker <wkc419@hotmail.com>
2020-04-13 13:43:36 +0100

    After chapter 102.  Enemies now being collided with particle system and destroyed.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Tooltip("Death Effects Object")] [SerializeField] GameObject deathFX;
+    [SerializeField] float enemyHealth = 50f;
+    // Start is called before the first frame update
+    void OnParticleCollision(GameObject other)
+    {
+        DestroyEnemy();
+    }
+
+    private void DestroyEnemy()
+    {
+        deathFX.SetActive(true);
+        MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        BoxCollider boxCollider = gameObject.GetComponent<BoxCollider>();
+        meshRenderer.enabled = false;
+        boxCollider.enabled = false;
+        Invoke("DestroyEnemyGameObject", 1f);
+    }
+    private void DestroyEnemyGameObject() //Referenced by string in DestroyEnemy method.
+    {
+        Destroy(gameObject);
+    }
+}
